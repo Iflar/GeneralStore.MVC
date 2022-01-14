0ffbdbb710bdc9c6c7f295373709a5d8d2a45057
iflar <classicsience@gmail.com>
2022-01-13 19:27:27 -0500

    Create, Edit, Delete, Detail product functionality added

--- GeneralStore.MVC/Models/Product.cs
+++ GeneralStore.MVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace GeneralStore.MVC.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "# In Stock")]
         public int InventoryCount { get; set; }
+        [Required]
         public double Price { get; set; }
+        [Required]
+        [Display(Name = "It is food")]
         public bool IsFood { get; set; }
     }
 }
