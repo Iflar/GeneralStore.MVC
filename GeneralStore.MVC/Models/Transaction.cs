b3fc6de32facf8249f1ed3464600cff1d805f202
iflar <classicsience@gmail.com>
2022-01-18 18:35:00 -0500

    added Transaction class + controller - transaction views WIP

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Required]
+        [ForeignKey(nameof(ProductID))]
+        public int ProductID { get; set; }
+        public List<Product> Products { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CustomerID))]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+        public double PriceTotal { get; set; }
+    }
+}
